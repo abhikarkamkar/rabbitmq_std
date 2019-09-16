fac4fa462d8b8e351b12fccf592e931053c42720
abhikarkamkar <41470976+abhikarkamkar@users.noreply.github.com>
2019-09-16 14:30:26 +0530

    Create RabbitMQ.h

---
+++ RabbitMQ.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
+
+#define USERNAME	"testuser"
+#define PASSWORD	"testuser"
+#define HOSTNAME	"10.10.97.135"
+#define PORT		5672
